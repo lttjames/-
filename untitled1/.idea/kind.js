2a9ae6ea6a1bbb7369fde396d91e2e6640df64e4
lttjames <33799375+lttjames@users.noreply.github.com>
2018-08-26 22:02:36 +0800

    mission for nmc

--- untitled1/.idea/kind.js
+++ untitled1/.idea/kind.js
@@ -1,5 +1,6 @@
 var tabs = document.querySelectorAll('#direction li a');
 var panels = document.querySelectorAll('#categray .kinds');
+var ppt = document.querySelectorAll('#content');
 
 for(var i = 0; i < tabs.length; i++) 
 {
@@ -15,9 +16,31 @@ function setTabHandler(tab, tabPos)
     {
       tabs[num].removeAttribute('class');
       panels[num].setAttribute('class', 'linestyle kinds');
+      ppt[num].setAttribute('class', 'location');
     }
 
     tab.setAttribute('class', 'itembeginning');
     panels[tabPos].setAttribute('class', 'kinds linestyle active1');
+    switch (tabPos)
+    {
+      case 0:
+        ppt[0].setAttribute('class', 'location active1');
+        break;
+      case 1:
+        ppt[2].setAttribute('class', 'location active1');
+        break;
+      case 2:
+        ppt[3].setAttribute('class', 'location active1');
+        break;
+      case 3:
+        ppt[4].setAttribute('class', 'location active1');
+        break;
+      case 4:
+        ppt[7].setAttribute('class', 'location active1');
+        break;
+      default:
+        ppt[8].setAttribute('class', 'location active1');
+        break;
+    }
   }
 }
--- untitled1/.idea/show.js
+++ untitled1/.idea/show.js
@@ -1,23 +1,43 @@
 var tabs1 = document.querySelectorAll('.kinds li a');
 var panels1 = document.querySelectorAll('#content');
 
-for(var i = 0; i < tabs1.length; i++) 
+for(var i = 0; i < 2; i++) 
 {
   var tab = tabs1[i];
   setTabHandler(tab, i);
 }
 
-function setTabHandler(tab, tabPos) 
+function setTabHandler(tab, tabPo) 
 {
   tab.onclick = function() 
   {
-    for(var num = 0; num < tabs1.length; num++) 
+    for(var num = 0; num < 2; num++) 
     {
       tabs1[num].removeAttribute('class');
-      panels1[tabPos].setAttribute('class', 'location')
+      panels1[num].setAttribute('class', 'location');
     }
 
     tab.setAttribute('class', 'itembeginning');
-    panels1[tabPos].setAttribute('class', 'location active1');
+    panels1[tabPo].setAttribute('class', 'location active1');
+  }
+}
+for(var j = 4; j < 7; j++)
+{
+  var tab1=tabs1[j];
+  links(tab1, j);
+}
+
+function links(tab1,tabPos1)
+{
+  tab1.onclick = function() 
+  {
+    for(var num = 4; num < 7; num++) 
+    {
+      tabs1[num].removeAttribute('class');
+      panels1[num].setAttribute('class', 'location');
+    }
+
+    tab1.setAttribute('class', 'itembeginning');
+    panels1[tabPos1].setAttribute('class', 'location active1');
   }
 }
